5166081735892aed40f7913b692cbc1925231bbf
Riad Benguella <riad.benguella@pmsipilot.com>
2012-06-07 14:13:09 +0200

    updating init command

--- src/Rizeway/Anchour/Console/Command/InitCommand.php
+++ src/Rizeway/Anchour/Console/Command/InitCommand.php
@@ -31,18 +31,79 @@ class InitCommand extends Command
 
     protected function getTemplate() {
         return <<<YAML
-#Here you can define your targets
-target:
-    description: "A default target"
-    connections:
-        #Here you can define your connections
 
-    steps:
-        #Here you can define your steps
-        -
-            type: "echo"
-            options:
-                message: "This is a default <info>echo</info> step"
+# Here you can define your connections
+connections:
+    MySSH:
+        options:
+            username: %username%
+            password: %password%
+            host: %host%
+
+# Here you can define your commands
+commands:
+
+    # Default deploy command using rsync
+    deploy:
+        description: "Deploy your project using rsync"        
+
+        # Here you can define your command steps
+        steps:
+            -
+                type: "echo"
+                options:
+                    message: "<info>Starting Rsync</info>"
+
+            -
+                type: "rsync"
+                options:
+                    key_file: %rsync_key_file%
+                connections:
+                    connection: MySSH
+
+            -
+                type: "echo"
+                options:
+                    message: "<info>Rsync Done</info>"
+
+    # Default save revision command
+    save:
+        description: "save a revision of your remote directory"
+        steps:
+            -
+                type: "ssh"
+                options:
+                    commands:
+                        - mkdir -p .anchour/revisions/%revision_name%
+                        - rsync --progress -a --exclude '.anchour/' ./ .anchour/revisions/%revision_name%
+                connections:
+                    connection: MySSH
+                    
+
+    # Default rollback revision command
+    rollback:
+        description: "Rollback you project to a saved revision"
+        steps:
+            -
+                type: "ssh"
+                options:
+                    commands:
+                        - cp -aR .anchour/revisions/%revision_name% ./
+                connections:
+                    connection: MySSH
+
+    # Default command to list your saved revisions
+    revisions:
+        description: "List your saved revisions"
+        steps:
+            -
+                type: "ssh"
+                options:
+                    commands:
+                        - ls .anchour/revisions
+                connections:
+                    connection: MySSH
+
 YAML;
     }
 }
