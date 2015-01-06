aa0ee20397a4b434fcdfc1e10dad377cb7697468
Riad Benguella <riad.benguella@pmsipilot.com>
2015-01-06 16:55:45 +0100

    change Anchour version

--- src/Rizeway/Anchour/Phar/stub.php
+++ src/Rizeway/Anchour/Phar/stub.php
@@ -3,7 +3,7 @@
 Phar::mapPhar('anchour.phar');
 
 if (!defined('ANCHOUR_VERSION')) {
-    define('ANCHOUR_VERSION', '0.1-phar');
+    define('ANCHOUR_VERSION', '1.1.0-phar');
 }
 
 require 'phar://anchour.phar/anchour.php';
